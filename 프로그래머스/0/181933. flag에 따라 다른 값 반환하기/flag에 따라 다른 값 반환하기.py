9f4cfdce7b88838279842d4808eba2439a588f87
sleepotato <koreavally@gmail.com>
2024-07-27 22:53:21 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.py
@@ -1,3 +1,4 @@
-def solution(a, b, flag):
-    if flag: return a+b
-    else: return a-b
+solution=lambda a,b,f:[a-b,a+b][f]
+# def solution(a, b, flag):
+#     if flag: return a+b
+#     else: return a-b
