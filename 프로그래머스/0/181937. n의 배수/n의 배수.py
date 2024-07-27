fcff42afc73a0a0ad704e5dab88395e4d0b3619b
sleepotato <koreavally@gmail.com>
2024-07-27 15:22:53 +0900

    [level 0] Title: n의 배수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.py
+++ 프로그래머스/0/181937. n의 배수/n의 배수.py
@@ -1,5 +1,6 @@
 def solution(num, n):
-    if num%n == 0:
-        return 1
-    elif num%n != 0:
-        return 0
+    # if num%n == 0:
+    #     return 1
+    # elif num%n != 0:
+    #     return 0
+    return int(num%n==0)
