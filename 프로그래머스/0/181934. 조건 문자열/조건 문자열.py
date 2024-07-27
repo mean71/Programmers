f1c6a642838f3c7a5a86dbe52930eaa15bb515e4
sleepotato <koreavally@gmail.com>
2024-07-27 17:32:12 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -1,5 +1,9 @@
 def solution(ineq, eq, n, m):
-    if ineq=="<" and eq=="=": return int(n<=m)
-    if ineq==">" and eq=="=": return int(n>=m)
-    if ineq=="<" and eq=="!": return int(n<m)
-    if ineq==">" and eq=="!": return int(n>m)
+    if n==m and eq=='=':return 1
+    if n>m and ineq=='>':return 1
+    if n<m and ineq=='<':return 1
+    else:return 0
+    # if ineq=="<" and eq=="=": return int(n<=m)
+    # if ineq==">" and eq=="=": return int(n>=m)
+    # if ineq=="<" and eq=="!": return int(n<m)
+    # if ineq==">" and eq=="!": return int(n>m)
