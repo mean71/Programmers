52ac8912633f36a1b0aa49a81d6764aed169fb5c
Mean71 Park <koreavally@gmail.com>
2024-08-08 01:03:49 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -0,0 +1,16 @@
+N ,X= map(int,input().strip().split())
+A = list(map(int,input().strip().split()))
+def test(x):
+  return 1<= x <= 10000
+def Xsize(x,y):
+  if x > y:
+    return y
+def sx(n,x,a):
+  prx = []
+  if test(n) and test(x):
+    for i in range(len(a)):
+      if Xsize(x,a[i]):
+        prx.append(a[i])
+  return prx
+
+print(*sx(N,X,A))
